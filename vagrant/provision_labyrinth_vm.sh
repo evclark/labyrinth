c281bd6d0916f1ee471cf1864ea1244bec60632d
Evan Clark <evan.bock.clark@gmail.com>
2017-03-19 21:39:21 -0600

    Changed vagrant file to use X window forwarding instead of displaying VirtualBox GUI, got proviisoning to install X forwarding and ROS successfully

--- vagrant/provision_labyrinth_vm.sh
+++ vagrant/provision_labyrinth_vm.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-#Install VirtualBox Guest Additions 
-sudo apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-sudo sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
+#Install X authentification so we can see GUIs on the guest machine
+sudo apt-get install -y xauth
 
-#Install Ubuntu GUI
-sudo apt-get install ubuntu-desktop
+#Install ROS and setup ROS environment
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install -y ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 
-#Install ROS
-# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-# sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-# sudo apt-get update
-#sudo apt-get install ros-kinetic-desktop-full
+#Install Python package manager (pip)
+sudo apt-get install -y python-pip
+
+
